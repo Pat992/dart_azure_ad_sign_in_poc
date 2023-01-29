8c8b48844160bbe8f81aafd6c31989a5a9a8b90d
Patrick Hettich <patrick_hettich@hotmail.com>
2023-01-29 19:23:35 +0100

    init

---
+++ dart_azure_ad_sign_in_poc_console/bin/dart_azure_ad_sign_in_poc_console.dart
@@ -0,0 +1,38 @@
+import 'package:dart_azure_ad_sign_in/dart_azure_ad_sign_in.dart';
+import 'package:dart_azure_ad_sign_in/src/dart_azure_ad_sign_in_base.dart';
+
+Future<void> main(List<String> arguments) async {
+  // Create instance of Azure SignIn, all parameters are optional.
+  final azureSignIn = AzureSignIn();
+
+  // Print the SignIn URL.
+  print(azureSignIn.authUri);
+
+  // Let the user to open the URL and signing in
+  // can also b cancelled with azureSignIn.cancelSignIn();
+  Token token = await azureSignIn.signIn();
+
+  // Print the token information
+  printToken(token: token, title: 'Initial Token');
+
+  // refresh an expired token
+  token = await azureSignIn.refreshToken(token: token);
+
+  // Print the updated token information
+  printToken(token: token, title: 'Refreshed Token');
+}
+
+void printToken({required Token token, required String title}) {
+  print(
+      '------------------------------------------------------------------------------------------------------------------------------------');
+  print(title);
+  print(
+      '------------------------------------------------------------------------------------------------------------------------------------');
+  print('Status: ${token.status}');
+  print('Error: ${token.error}');
+  print('Error Message: ${token.errorDescription}');
+  print('Refresh Token:');
+  print(token.accessToken);
+  print('Refresh Token:');
+  print(token.accessToken);
+}
---
+++ dart_azure_ad_sign_in_poc_console/test/dart_azure_ad_sign_in_poc_console_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_azure_ad_sign_in_poc_console/dart_azure_ad_sign_in_poc_console.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
