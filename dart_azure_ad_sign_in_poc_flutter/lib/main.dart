7b54c38ad732a86849b79e2dbfe498f4ec60e175
Patrick Hettich <patrick_hettich@hotmail.com>
2023-01-29 21:44:06 +0100

    flutter poc done, tested on windows

--- dart_azure_ad_sign_in_poc_flutter/lib/main.dart
+++ dart_azure_ad_sign_in_poc_flutter/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:dart_azure_ad_sign_in/dart_azure_ad_sign_in.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,103 +15,196 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SignInPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SignInPage> createState() => _SignInPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SignInPageState extends State<SignInPage> {
+  final azureSignIn = AzureSignIn(port: 5000);
+  late Token token;
+  bool isSignIn = false;
+  int status = 0;
+  String error = '';
+  String errorDesc = '';
+  String activeFrom = '';
+  String activeTo = '';
+  String authToken = '';
+  String refToken = '';
 
-  void _incrementCounter() {
+  Future<void> getToken() async {
+    if (await canLaunchUrl(Uri.parse(azureSignIn.authUri))) {
+      await launchUrl(Uri.parse(azureSignIn.authUri));
+      token = await azureSignIn.signIn();
+      tokenToFields();
+    }
+  }
+
+  Future<void> cancelGetToken() async {
+    await azureSignIn.cancelSignIn();
+  }
+
+  Future<void> refreshToken() async {
+    token = await azureSignIn.refreshToken(token: token);
+    tokenToFields();
+  }
+
+  void tokenToFields() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      status = token.status;
+      error = token.error;
+      errorDesc = token.errorDescription;
+      activeFrom = token.notBefore;
+      activeTo = token.expiresOn;
+      authToken = token.accessToken;
+      refToken = token.refreshToken;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: const Text('Sign In to Azure')),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: getToken,
+                  child: const Text('Sign In'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: cancelGetToken,
+                  child: const Text('Cancel Sign In'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: refreshToken,
+                  child: const Text('Refresh Token'),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      const SizedBox(
+                        width: 150,
+                        child: Text(
+                          'Status:',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(status.toString()),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      const SizedBox(
+                        width: 150,
+                        child: Text(
+                          'Error: ',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(error),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      const SizedBox(
+                        width: 150,
+                        child: Text(
+                          'Error Description: ',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(errorDesc),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      const SizedBox(
+                        width: 150,
+                        child: Text(
+                          'Token active from: ',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(
+                        DateTime.fromMillisecondsSinceEpoch(
+                                int.parse(activeFrom) * 1000)
+                            .toString(),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      const SizedBox(
+                        width: 150,
+                        child: Text(
+                          'Token active to: ',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(
+                        DateTime.fromMillisecondsSinceEpoch(
+                                int.parse(activeTo) * 1000)
+                            .toString(),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      const SizedBox(
+                        width: 150,
+                        child: Text(
+                          'Auth Token: ',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Flexible(
+                        child: Text(
+                          authToken,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      const SizedBox(
+                        width: 150,
+                        child: Text(
+                          'Refresh Token: ',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Flexible(child: Text(refToken)),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
